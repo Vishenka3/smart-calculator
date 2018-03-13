eb3b79f0d6d8fcf9d3f0f2a1b38e3a67c9801071
Alex <djdoktop@gmail.com>
2018-03-14 02:13:03 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,27 +1,54 @@
-class SmartCalculator {
+class SmartCalculator{
   constructor(initialValue) {
     // your implementation
+      this.arithmetic = [initialValue];
   }
 
   add(number) {
     // your implementation
+      this.arithmetic.push('+');
+      this.arithmetic.push(number);
+      return this;
   }
   
   subtract(number) {
     // your implementation
+      this.arithmetic.push('-');
+      this.arithmetic.push(number);
+      return this;
   }
 
   multiply(number) {
     // your implementation
+      this.arithmetic.push('*');
+      this.arithmetic.push(number);
+      return this;
   }
 
   devide(number) {
     // your implementation
+      this.arithmetic.push('/');
+      this.arithmetic.push(number);
+      return this;
   }
 
   pow(number) {
     // your implementation
+      if(this.arithmetic[this.arithmetic.length-1][2]===')'){
+          let numb = ~~this.arithmetic[this.arithmetic.length-1][1];
+          number=Math.pow(numb,number);
+          this.arithmetic[this.arithmetic.length-1] = this.arithmetic[this.arithmetic.length-1].replace(numb, number);
+      }else{
+          this.arithmetic.splice(this.arithmetic.length-1, 0, 'Math.pow(');
+          this.arithmetic.push(','+ number + ')');
+      }
+      return this;
   }
+
+  valueOf() {
+      return eval(this.arithmetic.join(' '));
+  }
+
 }
 
 module.exports = SmartCalculator;
